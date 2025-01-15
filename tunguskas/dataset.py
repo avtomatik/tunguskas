59fb9bb471416db9ccf591337675b79582588f85
aarchitektt <axel.schrijver@gmail.com>
2025-01-15 11:06:44 +0300

    Updated Archive Reading

--- tunguskas/dataset.py
+++ tunguskas/dataset.py
@@ -7,38 +7,40 @@ Created on Thu Oct 20 21:47:18 2022
 """
 
 from pathlib import Path
+from zipfile import ZipFile
 
-import rarfile
-from openpyxl import load_workbook
+import pandas as pd
 
 # =============================================================================
 # TODO: https://git-lfs.com
 # =============================================================================
 FILE_NAME = 'urovni_p_i_n_tunguski.rar'
+FILE_NAME = 'archive.zip'
 
 
-with rarfile.RarFile(
+with ZipFile(
     (
         Path(__file__).parent.parent
         .joinpath('data')
         .joinpath('raw')
         .joinpath(FILE_NAME)
     )
-) as rf:
-    for f in rf.namelist():
-        print(f)
+) as archive:
 
-    workbook = load_workbook(
-        rf.open('!Уровни постов.xlsx'),
-        # =====================================================================
-        # TODO: Fix the Bug Below
-        # =====================================================================
-        # =====================================================================
-        #     raise BadRarFile("Failed the read enough data: req=%d got=%d" % (orig, len(data)))
-        # rarfile.BadRarFile: Failed the read enough data: req=8331 got=51
-        # =====================================================================
-        read_only=True,
-        keep_links=False
-    )
-    sheet_names = workbook.sheetnames
-    print(sheet_names)
+    dfs = []
+
+    with archive.open('!Уровни постов.xlsx') as f:
+        df_streamgages = pd.read_excel(f)
+
+    for _ in range(2008, 2018):
+        df_pack = pd.read_html(archive.open(f'Нижняя Тунгуска {_}.xls'))
+        for df in df_pack:
+            dfs.append(df)
+
+    for _ in range(2008, 2018):
+        df_pack = pd.read_html(archive.open(f'Подкаменная Тунгуска {_}.xls'))
+        for df in df_pack:
+            dfs.append(df)
+
+print(df_streamgages)
+print(len(dfs))
