a77fecb2170fb2a50c15d9045de774a2304cc9d8
Alexander Mikhailov <extroper@yandex.ru>
2025-04-27 12:25:43 +0300

    Modularized Dataset Creation Script
    & Updated `requirements.txt`

--- tunguskas/dataset.py
+++ tunguskas/dataset.py
@@ -8,100 +8,111 @@ Created on Thu Oct 20 21:47:18 2022
 
 import datetime
 import re
-from zipfile import ZipFile
+import zipfile
 
 import pandas as pd
+from config import COLUMNS_RE_SHUFFLED, DATA_DIR, FILE_NAME, Shape
 
-from .config import COLUMNS_RE_SHUFFLED, DATA_DIR, FILE_NAME, Shape
 
-
-def get_date(row):
+def get_date(row: pd.Series):
     try:
         return datetime.date(int(row['year']), row['variable'], int(row[0]))
     except ValueError:
         return
 
 
-def swap_value(value):
+def swap_value(value: str) -> str:
     match = re.search(r'(?P<legend>\D+) (?P<reading>\d+)', value)
     if match:
         return f"{match.group('reading')} {match.group('legend')}"
     return value
 
 
-with ZipFile(DATA_DIR.joinpath('raw').joinpath(FILE_NAME)) as archive:
-
+def process_file(
+    archive: zipfile.ZipFile,
+    location: str,
+    year: int
+) -> list[pd.DataFrame]:
     dfs = []
 
+    with archive.open(f'{location} {year}.xls') as f:
+        df_pack = pd.read_html(f)
+
+        for chunk in df_pack:
+            if chunk.shape == Shape.STAMPS.value:
+                post_id, _, river_post, gauge_zero, _ = chunk.iloc[:, -1]
+            elif chunk.shape == Shape.DATA.value:
+                chunk.drop(range(2), inplace=True)
+                chunk.drop(chunk.tail(7).index, inplace=True)
+                df = pd.melt(
+                    chunk,
+                    id_vars=chunk.columns[0],
+                    value_vars=chunk.columns[1:],
+                    ignore_index=False
+                )
+                df['year'] = year
+                df['location'] = location
+                df['post_id'] = post_id
+                df['river_post'] = river_post
+                df['gauge_zero'] = gauge_zero
+                dfs.append(df)
+
+    return dfs
+
+
+def process_archive(archive: zipfile.ZipFile) -> pd.DataFrame:
+    all_dfs = []
+
+    location = 'Нижняя Тунгуска'
     for year in range(2008, 2018):
-        with archive.open(f'Нижняя Тунгуска {year}.xls') as f:
-            location = 'Lower Tunguska'
-            df_pack = pd.read_html(f)
+        all_dfs.extend(process_file(archive, location, year))
 
-            for chunk in df_pack:
-                if chunk.shape == Shape.STAMPS.value:
-                    post_id, _, river_post, gauge_zero, _ = chunk.iloc[:, -1]
-                elif chunk.shape == Shape.DATA.value:
-                    chunk.drop(range(2), inplace=True)
-                    chunk.drop(chunk.tail(7).index, inplace=True)
-                    df = pd.melt(
-                        chunk,
-                        id_vars=chunk.columns[0],
-                        value_vars=chunk.columns[1:],
-                        ignore_index=False
-                    )
-                    df['year'] = year
-                    df['location'] = location
-                    df['post_id'] = post_id
-                    df['river_post'] = river_post
-                    df['gauge_zero'] = gauge_zero
-                    dfs.append(df)
-
+    location = 'Подкаменная Тунгуска'
     for year in range(2008, 2018):
-        with archive.open(f'Подкаменная Тунгуска {year}.xls') as f:
-            location = 'Stony Tunguska'
-            df_pack = pd.read_html(f)
+        all_dfs.extend(process_file(archive, location, year))
 
-            for chunk in df_pack:
-                if chunk.shape == Shape.STAMPS.value:
-                    post_id, _, river_post, gauge_zero, _ = chunk.iloc[:, -1]
-                elif chunk.shape == Shape.DATA.value:
-                    chunk.drop(range(2), inplace=True)
-                    chunk.drop(chunk.tail(7).index, inplace=True)
-                    df = pd.melt(
-                        chunk,
-                        id_vars=chunk.columns[0],
-                        value_vars=chunk.columns[1:],
-                        ignore_index=False
-                    )
-                    df['year'] = year
-                    df['location'] = location
-                    df['post_id'] = post_id
-                    df['river_post'] = river_post
-                    df['gauge_zero'] = gauge_zero
-                    dfs.append(df)
+    return pd.concat(all_dfs)
 
-df = pd.concat(dfs)
 
-df['date'] = df.apply(get_date, axis=1)
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    df['date'] = df.apply(get_date, axis=1)
 
-df.dropna(inplace=True)
+    df.dropna(inplace=True)
 
-df['gauge_zero'] = df['gauge_zero'].apply(pd.to_numeric)
-df['value'] = df['value'].apply(swap_value)
+    df['gauge_zero'] = df['gauge_zero'].apply(pd.to_numeric)
+    df['value'] = df['value'].apply(swap_value)
 
-df.drop(df.columns[range(2)], axis=1, inplace=True)
-df.drop(df.columns[range(1, 2)], axis=1, inplace=True)
+    df.drop(df.columns[range(2)], axis=1, inplace=True)
+    df.drop(df.columns[range(1, 2)], axis=1, inplace=True)
 
-df[['value', 'legend']] = df['value'].str.extract(
-    r'(?P<reading>\d+)(?P<legend> \D+)?',
-    expand=True
-)
+    df[['value', 'legend']] = df['value'].str.extract(
+        r'(?P<reading>\d+)(?P<legend> \D+)?',
+        expand=True
+    )
 
-df['legend'] = df['legend'].str.strip()
-df['value'] = df['value'].apply(pd.to_numeric, downcast='integer')
+    df['legend'] = df['legend'].str.strip()
+    df['value'] = df['value'].apply(pd.to_numeric, downcast='integer')
 
-df[COLUMNS_RE_SHUFFLED].to_csv(
-    DATA_DIR.joinpath('processed').joinpath('dataset.csv'),
-    index=False
-)
+    return df
+
+
+def save_processed_data(df: pd.DataFrame) -> None:
+    df[COLUMNS_RE_SHUFFLED].to_csv(
+        DATA_DIR.joinpath('processed').joinpath('dataset.csv'),
+        index=False
+    )
+
+
+def main():
+    with zipfile.ZipFile(
+        DATA_DIR.joinpath('raw').joinpath(FILE_NAME)
+    ) as archive:
+        (
+            process_archive(archive)
+            .pipe(clean_data)
+            .pipe(save_processed_data)
+        )
+
+
+if __name__ == '__main__':
+    main()
